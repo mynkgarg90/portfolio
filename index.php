12b3a8b1902f67fb23692711c45a84db735a2ee7
mynkgarg90 <mynkgarg90@gmail.com>
2026-09-08 01:42:50 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
 // ============================================================
 // SINGLE FILE 3D PORTFOLIO WEBSITE
 // File: index.php
